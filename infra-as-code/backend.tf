b688476bf7e8d4fd100deacb956e5d5532e3199b
gPass0s <guilhermepassos@outlook.com>
2022-02-10 15:22:51 -0300

    Changes the key name in terraform backend.tf file

--- infra-as-code/backend.tf
+++ infra-as-code/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
     bucket      = "gpass0s-aws-cicd-pipeline"
-    key         = "gpass0s-aws-cicd-pipeline.tfstate"
+    key         = "terraform-assests/terraform.tfstate"
     region      = "us-east-1"
     encrypt     = true
-    role_arn    = "arn:aws:iam::777455183230:role/gpass0s-terraform-role"
   }
 }
