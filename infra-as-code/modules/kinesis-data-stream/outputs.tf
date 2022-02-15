6e85ae490e2180ede104db3ca2fe1ed83315170b
gPass0s <guilhermepassos@outlook.com>
2022-02-15 17:54:45 -0300

    Renames output variables of kinesis data stream terraform module

--- infra-as-code/modules/kinesis-data-stream/outputs.tf
+++ infra-as-code/modules/kinesis-data-stream/outputs.tf
@@ -1,9 +1,9 @@
 output "arn" {
   description = "kinesis analytics arn"
-  value       = aws_kinesis_stream.credit_kinesis_stream.arn
+  value       = aws_kinesis_stream.kinesis_stream.arn
 }
 
 output "name" {
   description = "kinesis analytics resource name"
-  value       = aws_kinesis_stream.credit_kinesis_stream.name
+  value       = aws_kinesis_stream.kinesis_stream.name
 }
