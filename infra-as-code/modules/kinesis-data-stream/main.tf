4e2ce9383706eca2e8de66513d6f0f162d3e32ed
gPass0s <guilhermepassos@outlook.com>
2022-02-15 15:41:11 -0300

    Renames terraform resource name

--- infra-as-code/modules/kinesis-data-stream/main.tf
+++ infra-as-code/modules/kinesis-data-stream/main.tf
@@ -2,7 +2,7 @@ locals {
   RESOURCE_NAME = "${var.PROJECT_NAME}-${var.ENV}-${var.RESOURCE_SUFFIX}"
 }
 
-resource "aws_kinesis_stream" "credit_kinesis_stream" {
+resource "aws_kinesis_stream" "kinesis_stream" {
   name             = local.RESOURCE_NAME
   shard_count      = var.NUMBER_OF_SHARDS
   retention_period = var.RETENTION_PERIOD
