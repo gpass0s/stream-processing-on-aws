eeea2128a8f9a6eef8629c0dd8ae567ff32b4646
gPass0s <guilhermepassos@outlook.com>
2022-02-15 15:42:03 -0300

    Uploads the reference table to S3

--- infra-as-code/main.tf
+++ infra-as-code/main.tf
@@ -1,3 +1,5 @@
+#region S3
+#Creates a S3 bucket
 module "codepipeline-artifacts-bucket" {
   source          = "./modules/s3"
   ENV             = "prd"
@@ -5,3 +7,13 @@ module "codepipeline-artifacts-bucket" {
   RESOURCE_SUFFIX = "codepipeline-artifacts"
   AWS_TAGS        = "streaming-processing"
 }
+
+#Uploads the reference table to S3
+resource "aws_s3_bucket_object" "reference-table" {
+  key = "reference-table/upload-from-terraform/users_annual_income.csv"
+  bucket = module.codepipeline-artifacts-bucket.id
+  source = "../kda-utils/users_annual_income.csv"
+  server_side_encryption = "AES256"
+  etag = filemd5("../kda-utils/users_annual_income.csv")
+}
+#endregion
