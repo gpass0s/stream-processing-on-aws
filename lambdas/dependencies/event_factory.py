8393fc82fac330a48b6b3d416145f5a63c348b5b
gPass0s <guilhermepassos@outlook.com>
2022-03-02 03:06:45 -0300

    Removes document number field from events and generates message attributes in factory

--- lambdas/dependencies/event_factory.py
+++ lambdas/dependencies/event_factory.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8 -*-
 """
 Created on Tue Feb 22 01:42 BRT 2022
-Updated on Wed Feb 23 01:08 BRT 2022
+Updated on Tue Mar 01 02:25  BRT 2022
 author: https://github.com/gpass0s/
 This module implements an event factory
 """
@@ -19,7 +19,6 @@ class EventFactory(ABC):
     def generate_event(
             self,
             fake_data_generator: Faker,
-            document_number: str,
             client_personal_info: dict
     ) -> dict:
         pass
@@ -30,18 +29,18 @@ class CreateAddressEvent(EventFactory):
     def generate_event(
             self,
             fake_data_generator: Faker,
-            document_number: str,
             client_personal_info: dict
     ) -> dict:
         event = {}
         data = {}
 
         fake_hash = f"{fake_data_generator.hexify()}{fake_data_generator.hexify()}{fake_data_generator.hexify()}"
-        event["event_id"] = fake_hash
-        event["timestamp"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        event["eventId"] = fake_hash
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+        event["timestamp"] = timestamp
         event["eventName"] = "client_address"
 
-        data["documentNumber"] = document_number
+        data["ssn"] = client_personal_info["ssn"]
         data["clientName"] = client_personal_info["name"]
         data["accountId"] = client_personal_info["accountId"]
 
@@ -60,7 +59,22 @@ class CreateAddressEvent(EventFactory):
         data["addresses"] = addresses
         event["data"] = data
 
-        return event
+        message_attributes = {
+            "eventId": {
+                "StringValue": fake_hash,
+                "DataType": "String"
+            },
+            "timestamp": {
+                "StringValue": timestamp,
+                "DataType": "String"
+            },
+            "eventName": {
+                "StringValue": "client_address",
+                "DataType": "String"
+            }
+        }
+
+        return event, message_attributes
 
 
 class CreateRiskAnalysisEvent(EventFactory):
@@ -68,19 +82,18 @@ class CreateRiskAnalysisEvent(EventFactory):
     def generate_event(
             self,
             fake_data_generator: Faker,
-            document_number: str,
             client_personal_info: dict
     ) -> dict:
         event = {}
         data = {}
 
         fake_hash = f"{fake_data_generator.hexify()}{fake_data_generator.hexify()}{fake_data_generator.hexify()}"
-        event["event_id"] = fake_hash
-        event["timestamp"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        event["eventId"] = fake_hash
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+        event["timestamp"] = timestamp
         event["eventName"] = "client_risk_analysis"
 
         data["ssn"] = client_personal_info["ssn"]
-        data["documentNumber"] = document_number
         data["yearMonthReference"] = datetime.datetime.now().strftime("%Y%m")
         risk_origin_distribution = ["M", "M", "M", "M", "N", "N", "N", "L", "O", "P"]
         data["riskOrigin"] = risk_origin_distribution[random.randint(0, 9)]
@@ -94,7 +107,22 @@ class CreateRiskAnalysisEvent(EventFactory):
 
         event["data"] = data
 
-        return event
+        message_attributes = {
+            "eventId": {
+                "StringValue": fake_hash,
+                "DataType": "String"
+            },
+            "timestamp": {
+                "StringValue": timestamp,
+                "DataType": "String"
+            },
+            "eventName": {
+                "StringValue": "client_risk_analysis",
+                "DataType": "String"
+            }
+        }
+
+        return event, message_attributes
 
 
 class CreateHistoryEvent(EventFactory):
@@ -102,18 +130,18 @@ class CreateHistoryEvent(EventFactory):
     def generate_event(
             self,
             fake_data_generator: Faker,
-            document_number: str,
             client_personal_info: dict
     ) -> dict:
         event = {}
         data = {}
 
         fake_hash = f"{fake_data_generator.hexify()}{fake_data_generator.hexify()}{fake_data_generator.hexify()}"
-        event["event_id"] = fake_hash
-        event["timestamp"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        event["eventId"] = fake_hash
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+        event["timestamp"] = timestamp
         event["eventName"] = "client_history"
 
-        data["documentNumber"] = document_number
+        data["ssn"] = client_personal_info["ssn"]
         creation_date = fake_data_generator.date_between_dates(
             datetime.date(2019, 1, 1),
             datetime.date.today()
@@ -127,5 +155,20 @@ class CreateHistoryEvent(EventFactory):
         ).strftime("%Y-%m-%d")
 
         event["data"] = data
+
+        message_attributes = {
+            "eventId": {
+                "StringValue": fake_hash,
+                "DataType": "String"
+            },
+            "timestamp": {
+                "StringValue": timestamp,
+                "DataType": "String"
+            },
+            "eventName": {
+                "StringValue": "client_history",
+                "DataType": "String"
+            }
+        }
 
         return event
