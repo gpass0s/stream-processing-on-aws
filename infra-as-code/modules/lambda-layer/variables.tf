e831c5cf44c2b76bb4388b20d70843f28c8033bc
gPass0s <guilhermepassos@outlook.com>
2022-03-17 03:03:36 -0300

    Improve variable.tf file of lambda-layer terraform module

--- infra-as-code/modules/lambda-layer/variables.tf
+++ infra-as-code/modules/lambda-layer/variables.tf
@@ -3,6 +3,7 @@ variable ENV {}
 variable RESOURCE_SUFFIX {}
 variable BUILDER_SCRIPT_PATH {}
 variable REQUIREMENTS_PATH {}
+variable PACKAGE_OUTPUT_NAME {}
 variable PYTHON_RUNTIME {
   default = "python3.8"
 }
