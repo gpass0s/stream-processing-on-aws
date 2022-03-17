5218fb9424f660786ed86daf8fc5ceb31dd14e2b
gPass0s <guilhermepassos@outlook.com>
2022-03-17 03:03:00 -0300

    Improve main.tf file of lambda-layer terraform module

--- infra-as-code/modules/lambda-layer/main.tf
+++ infra-as-code/modules/lambda-layer/main.tf
@@ -4,9 +4,9 @@ locals {
 
 module "build" {
   source = "./build"
-  BUILD_SETTINGS  {
+  BUILD_SETTINGS = {
     builder_script_path = var.BUILDER_SCRIPT_PATH
-    function_name       = var.RESOURCE_SUFFIX
+    layer_name          = var.RESOURCE_SUFFIX
     runtime             = var.PYTHON_RUNTIME
     package_output_name = var.PACKAGE_OUTPUT_NAME
     requirements_path   = var.REQUIREMENTS_PATH
@@ -18,5 +18,5 @@ resource "aws_lambda_layer_version" "python-layer" {
   layer_name          = var.RESOURCE_SUFFIX
   source_code_hash    = filebase64sha256(module.build.package_output_path)
   compatible_runtimes = [var.PYTHON_RUNTIME]
-  depends_on = [module.build.install_python_dependencies]
+  depends_on          = [module.build.install_python_dependencies]
 }
