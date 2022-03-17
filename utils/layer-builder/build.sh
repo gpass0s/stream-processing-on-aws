c15577487aef3377bb573d32c1af29c9086f510c
gPass0s <guilhermepassos@outlook.com>
2022-03-17 03:08:49 -0300

    Move layer-build folder into utils folder

--- utils/layer-builder/build.sh
+++ utils/layer-builder/build.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-echo "Executing create_pkg.sh..."
+echo "Executing build.sh..."
 
-cd $root_directory
-root_directory_path=$(pwd)
+if [[ $root_directory ]]; then # if the root_directory is set
+  cd $root_directory
+fi
+
+if [[ -z $package_output_name ]]; then # if package_output_name is not set
+  package_output_name=python_dependencies
+fi
+
+if [[ -z $function_name ]]; then # if function_name is not set
+  function_name=layer
+fi
+
+if [[ -z $runtime ]]; then # if runtime is not set
+  runtime=python3.8
+fi
+
+if [[ -z $requirements_path ]]; then # if requirements_path is not set
+  requirements_path=requirements.txt
+fi
+
 mkdir $package_output_name
 
 # Create and activate virtual environment...
 virtualenv -p $runtime env_$function_name
-source $root_directory_path/env_$function_name/bin/activate
+source env_$function_name/bin/activate
 
 # Installing python dependencies...
-FILE=$root_directory_path/$requirements_path
+FILE=$requirements_path
 
 if [ -f "$FILE" ]; then
   echo "Installing dependencies..."
@@ -27,23 +45,21 @@ deactivate
 
 # Create deployment package...
 echo "Creating deployment package..."
-cd env_$function_name/lib/$runtime/site-packages/
-cp -r . $root_directory_path/$package_output_name
-cp -r $root_directory_path/lambdas/dependencies $root_directory_path/$package_output_name
+
+cp -r env_$function_name/lib/$runtime/site-packages/. $package_output_name
+cp -r lambdas/dependencies $package_output_name
 
 # Removing virtual environment folder...
 echo "Removing virtual environment folder..."
-cd $root_directory_path
 rm -rf $env_$function_name
 
 # Zipping deployment package...
 echo "Zipping deployment package..."
-cd $package_output_name
-zip -r $root_directory_path/$package_output_name.zip .
+rm -rf $package_output_name/*pandas*
+zip -r $package_output_name.zip $package_output_name/.
 
 # Removing deployment package folder
 echo "Removing deployment package folder..."
-cd $root_directory_path
 rm -rf $package_output_name
 
 echo "Finished script execution!"
