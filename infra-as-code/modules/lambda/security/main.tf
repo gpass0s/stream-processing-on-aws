534f58d5467be1705983e100213808b6e22802d8
gPass0s <guilhermepassos@outlook.com>
2022-03-29 23:54:03 -0300

    Add policy to lambda module that allows send data to kds

--- infra-as-code/modules/lambda/security/main.tf
+++ infra-as-code/modules/lambda/security/main.tf
@@ -83,4 +83,13 @@ data "aws_iam_policy_document" "lambda_role_policy" {
       "arn:aws:logs:*:*:log-group:/aws/lambda/${var.RESOURCE_NAME}:*"
     ]
   }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kinesis:PutRecord"
+    ]
+    resources = [
+      "*"
+    ]
+  }
 }
