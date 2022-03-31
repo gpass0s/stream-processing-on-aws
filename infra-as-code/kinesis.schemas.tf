94a160e99d98594c98848b6b8f3376b6709b1804
gPass0s <guilhermepassos@outlook.com>
2022-03-31 02:55:53 -0300

    Create terraform file for kinesis data analytics input schemas

---
+++ infra-as-code/kinesis.schemas.tf
@@ -0,0 +1,66 @@
+variable KDA_STREAM_INPUT_SCHEMA {
+  type= any
+  default = [
+    {
+      mapping = "$.ssn",
+      name = "ssn",
+      sql_type = "VARCHAR(32)"
+    },
+    {
+      mapping = "$.streamProcTimestamp",
+      name = "streamProcTimestamp",
+      sql_type = "VARCHAR(32)"
+    },
+    {
+      mapping = "$.clientAddressEvent",
+      name = "clientAddressEvent",
+      sql_type = "VARCHAR(32768)"
+    },
+    {
+      mapping  = "$.clientRiskAnalysisEvent",
+      name     = "clientRiskAnalysisEvent",
+      sql_type = "VARCHAR(32768)"
+    },
+    {
+      mapping  = "$.clientHistoryEvent",
+      name     = "clientHistoryEvent",
+      sql_type = "VARCHAR(32768)"
+    }
+  ]
+}
+
+variable KDA_REFERENCE_TABLE_SCHEMA {
+  type= any
+  default = [
+    {
+      mapping = "$.ssn",
+      name = "ssn",
+      sql_type = "VARCHAR(32)"
+    },
+    {
+      mapping = "$.accountId",
+      name = "accountId",
+      sql_type = "VARCHAR(32)"
+    },
+    {
+      mapping = "$.name",
+      name = "name",
+      sql_type = "VARCHAR(128)"
+    },
+    {
+      mapping = "$.date_of_birth",
+      name = "date_of_birth",
+      sql_type = "date_of_birth(32)"
+    },
+    {
+      mapping = "$.occupation",
+      name = "occupation",
+      sql_type = "occupation(32)"
+    },
+    {
+      mapping = "$.annual_income",
+      name = "annual_income",
+      sql_type = "VARCHAR(32)"
+    }
+  ]
+}
