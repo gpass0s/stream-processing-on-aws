3968887abd7fdb8f59a037aaacfc62b77ba81d5a
gPass0s <guilhermepassos@outlook.com>
2022-04-02 11:52:30 -0300

    Add print line in lambda/event_proudcer.py

--- lambdas/event_producer.py
+++ lambdas/event_producer.py
@@ -47,6 +47,7 @@ def threads_controller(sns_client, sns_topic_arn, fake_data_generator, event_gen
                 MessageAttributes=message_attributes,
                 MessageStructure="string"
             )
+            print(f"[INFO] Event created: {event}")
             time.sleep(random.uniform(0.5, 3.5))
 
 
