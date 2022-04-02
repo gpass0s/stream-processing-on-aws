6a3f4b5dd8607f1427b5e408d7020be84bdffec6
gPass0s <guilhermepassos@outlook.com>
2022-04-02 10:36:39 -0300

    Add trigger for kinesis in event-source module

--- infra-as-code/modules/lambda/event-source/main.tf
+++ infra-as-code/modules/lambda/event-source/main.tf
@@ -1,6 +1,13 @@
 resource "aws_lambda_event_source_mapping" "sqs" {
   batch_size       = 10
-  count            = var.SETTINGS["protocol"] == "sqs" ? 1 : 0
+  count            = var.SETTINGS["trigger"] == "sqs" ? 1 : 0
   event_source_arn = var.SETTINGS["event_source_arn"]
   function_name    = var.SETTINGS["lambda_arn"]
+}
+
+resource "aws_lambda_event_source_mapping" "kinesis" {
+  count                              = var.SETTINGS["trigger"] == "kinesis" ? 1 : 0
+  event_source_arn                   = var.SETTINGS["event_source_arn"]
+  function_name                      = var.SETTINGS["lambda_arn"]
+  starting_position                  = "LATEST"
 }
